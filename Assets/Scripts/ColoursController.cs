845213b59f0f2e39b7882041cad976760933f698
Andrey Goldgamer <andrey.goldgamer@behalf.com>
2017-08-31 18:24:14 +0300

    First push

---
+++ Assets/Scripts/ColoursController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoursController : MonoBehaviour {
+
+	private SpriteRenderer sprite;
+	public int thisButtonNum;
+	private GameManager GM;
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer> ();
+		GM = FindObjectOfType<GameManager> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1f);
+	}
+
+	void OnMouseUp(){
+		sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0.5f);
+		GM.ButtonPressed (thisButtonNum);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+	public SpriteRenderer[] colours;
+	private int colourSelect;
+	public float stayLit;
+	private float stayLitCounter;
+	public float waitBetweenLights;
+	private float waitBetweenCounter;
+
+	private bool shouldBeLit;
+	private bool shouldBeDark;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shouldBeLit) {
+			stayLitCounter -= Time.deltaTime;
+			if (stayLitCounter < 0) {
+				colours [colourSelect].color = new Color (colours [colourSelect].color.r, colours [colourSelect].color.g, colours [colourSelect].color.b, 0.5f);
+				shouldBeLit = false;
+			}
+		}
+	}
+
+	public void StartGame(){
+		colourSelect = Random.Range (0, colours.Length);
+		colours[colourSelect].color = new Color(colours[colourSelect].color.r, colours[colourSelect].color.g, colours[colourSelect].color.b, 1f);
+		stayLitCounter = stayLit;
+		shouldBeLit = true;
+	}
+
+	public void ButtonPressed(int buttonPressed){
+		if (colourSelect == buttonPressed) {
+			Debug.Log("Correct");
+		}else{
+			Debug.Log("Wrong");
+		}
+	}
+
+}
